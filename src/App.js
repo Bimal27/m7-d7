e7cf03dde0da4fa06bc2a68bca8ff95a6818d10a
Bimal27 <bimalsah1996@gmail.com>
2021-10-21 11:52:42 +0545

    favoutites

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import MainSearch from './components/MainSearch';
 import CompanySearchResults from './components/CompanySearchResults';
-import { BrowserRouter, Route } from 'react-router-dom'
+import Favourites from './components/Favourites';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
 
 function App() {
   return (
     <BrowserRouter>
-      <Route exact path='/' component={MainSearch} />
-      <Route exact path='/:companyName' component={CompanySearchResults} />
+    <Switch>
+        <Route exact path='/' component={MainSearch} />
+        <Route exact path='/favourites' component={Favourites} />
+        <Route exact path='/:companyName' component={CompanySearchResults} />
+      </Switch>
     </BrowserRouter>
   );
 }
